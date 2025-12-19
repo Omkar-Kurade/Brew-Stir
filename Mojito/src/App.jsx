aa01aca46c5d89b6fb233711c6e099a45b85b198
Omkar-Kurade <omkarkurade14@gmail.com>
2025-12-19 22:23:27 +0530

    Implemented About Section and Menu

--- Mojito/src/App.jsx
+++ Mojito/src/App.jsx
@@ -3,6 +3,8 @@ import gsap from 'gsap'
 import { ScrollTrigger , SplitText } from 'gsap/all'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Cocktails from './components/Cocktails'
+import About from './components/About'
 
 gsap.registerPlugin(ScrollTrigger , SplitText)
 
@@ -13,6 +15,8 @@ function App() {
     <main>
       <Navbar/>
       <Hero/>
+      <Cocktails/>
+      <About/>
     </main>
     </>
   )
