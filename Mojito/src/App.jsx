75ffb1ebd13ffd026ba7c715318f6e27a32c9f02
Omkar-Kurade <omkarkurade14@gmail.com>
2025-12-19 16:14:11 +0530

    initial commit

---
+++ Mojito/src/App.jsx
@@ -0,0 +1,18 @@
+import gsap from 'gsap'
+import './App.css'
+import { ScrollTrigger , SplitText } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger , SplitText)
+
+function App() {
+ 
+  return (
+    <>
+     <div className='flex-center h-[100vh]'>
+      <h1>Hello GSAP</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
