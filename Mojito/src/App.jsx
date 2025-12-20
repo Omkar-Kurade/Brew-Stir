3c3c7cec4926e5dbca22be132009d46ac57912f8
Omkar-Kurade <omkarkurade14@gmail.com>
2025-12-20 17:43:55 +0530

    Implemented Contact section

--- Mojito/src/App.jsx
+++ Mojito/src/App.jsx
@@ -1,26 +1,29 @@
-import gsap from 'gsap'
-import { ScrollTrigger , SplitText } from 'gsap/all'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Cocktails from './components/Cocktails'
-import About from './components/About'
-import Art from './components/Art'
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
-gsap.registerPlugin(ScrollTrigger , SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
- 
   return (
     <>
-    <main>
-      <Navbar/>
-      <Hero/>
-      <Cocktails/>
-      <About/>
-      <Art/>
-    </main>
+      <main>
+        <Navbar />
+        <Hero />
+        <Cocktails />
+        <About />
+        <Art />
+        <Menu />
+        <Contact />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
